d7de897ef7297c88738d916ed79502b150dec2b6
Shunichi Ikegami <sike.tm@gmail.com>
2016-10-24 09:32:16 +0900

    Enable basic auth if ENV['BASIC_AUTH_USER'] set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASS'] unless Rails.env.test?
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASS'] if ENV['BASIC_AUTH_USER'].present? && !Rails.env.test?
 end
